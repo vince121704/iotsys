ca917ad54d07ea400f36168821b713be41418487
vince121704 <135985071+vince121704@users.noreply.github.com>
2026-05-01 23:09:22 +0800

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
 
 // -------- ADAFRUIT IO MQTT CONFIG --------
 const AIO_USERNAME = "vince17";
-const AIO_KEY      = "aio_mHRb586J17oOxuzoQHaMFnMjCmeG";
+const AIO_KEY      = "aio_nuHW29lc1lW8TgklAVXMCi4ZODUc";
 const AIO_WS_URL   = "wss://io.adafruit.com/mqtt/";
 
 // Feed keys (must match what the Arduino publisher uses)
